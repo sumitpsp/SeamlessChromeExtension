85fbc2f9b5628b8605375f817e105677452ec39f
Sumit Pasupalak <pasupalaks@gmail.com>
2013-09-24 08:46:25 -0400

    Committing

--- main.js
+++ main.js
@@ -1,10 +1,28 @@
 console.log("Crocky");
 
+chrome.tabs.onActivated.addListener( function(info) {
+    chrome.tabs.query({'active': true, 'windowId': chrome.windows.WINDOW_ID_CURRENT},
+   function(tabs){
+      //alert(tabs[0].url);
+      var url = "http://localhost:11111/activeurl";
+      var req = new XMLHttpRequest();
+      req.open('POST', url, true);
+      req.setRequestHeader('Content-type', 'application/txt');
+      req.onload = function() {
+            console.log("Response Received");
+        }
+      var senddata = "{\"url\": \"" + escape(tabs[0].url) + "\", \"title\": \"" + tabs[0].title + "\" }";
+      req.send(senddata);
+    
+    }
+    );
+});
+
 chrome.runtime.onConnect.addListener(function(port) {
 	console.assert(port.name == "sendurl"); 
 	chrome.tabs.query({'active': true, 'windowId': chrome.windows.WINDOW_ID_CURRENT},
    function(tabs){
-      alert(tabs[0].url);
+      //alert(tabs[0].url);
 	  var url = "http://localhost:11111/activeurl";
 	  var req = new XMLHttpRequest();
 	  req.open('POST', url, true);
@@ -12,7 +30,7 @@ chrome.runtime.onConnect.addListener(function(port) {
 	  req.onload = function() {
 			console.log("Response Received");
 		}
-	  var senddata = "\"" + tabs[0].url + "\"";
+	  var senddata = "{\"url\": \"" + escape(tabs[0].url) + "\", \"title\": \"" + tabs[0].title + "\" }";
 	  req.send(senddata);
     
 	}
